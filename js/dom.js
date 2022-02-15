280dc4198e2e464e952466420c16ee9ebf468414
Ahmed Qeshta <ahmedqeshta1999@gmail.com>
2022-02-15 20:29:37 +0200

    Create Body Search html and css and make it responsive Related #11

--- js/dom.js
+++ js/dom.js
@@ -1,12 +1,9 @@
 const navBarList = document.querySelector("#navbar-list");
 
-
 const addListener = (selector, eventName, callback) => {
     document.querySelector(selector).addEventListener(eventName, callback);
 };
 
-
-
-addListener(".bar","click",()=>{
-    navBarList.classList.toggle('nav-list-hide')
+addListener(".bar", "click", () => {
+    navBarList.classList.toggle("nav-list-hide");
 });
