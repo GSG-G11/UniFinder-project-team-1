763070cbde549ec9c3a15966b40476256cfd9984
nadabassam <nada.b.abu.zaid@gmail.com>
2022-02-17 10:12:50 +0200

    Fix Issues

--- js/fetch.js
+++ js/fetch.js
@@ -6,21 +6,18 @@
     });
 */
 const fetch = (method, url, callback) => {
-    let xhr = new XMLHttpRequest();
+    const xhr = new XMLHttpRequest();
 
-    xhr.addEventListener("load", function () {
-        if (xhr.readyState == 4 ) {
-           if ( xhr.status === 200) {
+    xhr.onreadystatechange = () => {
+        if (xhr.readyState == 4) {
+            if (xhr.status === 200) {
                 let response = JSON.parse(xhr.responseText);
                 return callback(response);
-           }else if ( xhr.status === 404){
-                window.location.href="../html/error.html";
-           }
-        } else {
-            window.location.href="../html/error.html";
+            } else if (xhr.status === 404) {
+                window.location.href = "../html/error.html";
+            }
         }
-    });
-
+    }
     xhr.open(method, url, false);
     xhr.send();
 };
--- js/info.js
+++ js/info.js
@@ -1,75 +1,7 @@
 const convertStringToObject = (object) => {
     return JSON.parse(object);
 };
-
-const handleGeneralInfo = () => {
-    const { name, flag, currency, language, callingCode, region } =
-    convertStringToObject(localStorage.getItem("generalInformation"));
-
-    let infoContainer = querySelector(".all-info");
-
-    let infoHead = createElement("div", "info-head", infoContainer);
-
-    let countryImg = createElement("img", "country-img", infoHead);
-    countryImg.src = flag;
-
-    let countryName = createElement("p", "coun-name", infoHead);
-    countryName.textContent = name;
-
-    // General Div
-    let infoDiv = createElement("div", "info-div", infoContainer);
-
-    // First Row
-    let firstRow = createElement("div", "first-row", infoDiv);
-
-    // Currency
-    let rowInfo1 = createElement("div", "row-info", firstRow);
-    createElement("i", "fas fa-money-bill-wave icons", rowInfo1);
-    let currName = createElement("p", "currency", rowInfo1);
-    currName.textContent = currency;
-
-    // Languages
-    let rowInfo2 = createElement("div", "row-info", firstRow);
-    createElement("i", "far fa-comment-dots icons", rowInfo2);
-    let langName = createElement("p", "lange", rowInfo2);
-    langName.textContent = language;
-
-    // Second Row
-    let secondRow = createElement("div", "second-row", infoDiv);
-
-    // Code
-    let rowInfo3 = createElement("div", "row-info", secondRow);
-    createElement("i", "fas fa-phone-alt icons", rowInfo3);
-    let code = createElement("p", "code", rowInfo3);
-    code.textContent = `+ ${callingCode}`;
-
-    // Region
-    let rowInfo4 = createElement("div", "row-info", secondRow);
-    createElement("i", "fas fa-globe-americas icons", rowInfo4);
-
-    let regionName = createElement("p", "region", rowInfo4);
-    regionName.textContent = region;
-};
 handleGeneralInfo();
-
-const handleUniCards = () => {
-    const universitiesArray = convertStringToObject(localStorage.getItem("universitiesStorage"));
-
-    universitiesArray.forEach(({ name, website }) => {
-
-        let uniContainer = querySelector(".uni-container");
-
-        let cardBtn = createElement("button", "card-btn", uniContainer);
-        cardBtn.setAttribute("onclick", `window.open('${website}', '_blank')`);
-
-        let card = createElement("div", "card", cardBtn);
-
-        createElement("i", "far fa-school", card);
-
-        let uniName = createElement("p", "uni-name", card);
-        uniName.textContent = name;
-    });
-};
 handleUniCards();
 
 // -------------- Font awesome script -------------------------
